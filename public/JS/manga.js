91e8024d337755bf3ac518e53a36721186f767bd
ShadowStudioEnterprise <shadowstudioenterprise@gmail.com>
2022-02-23 08:57:58 +0100

    last update

--- public/JS/manga.js
+++ public/JS/manga.js
@@ -1,67 +1,50 @@
-//UPDATE `manga` SET `puntuacion` = '5' WHERE `manga`.`id` = 2; 
 $(document).ready(function(){
     //https://codepen.io/depy/pen/vEWWdw  
     /* 1. Visualizing things on Hover - See next part for action on click */
     $('#stars li').on('mouseover', function(){
       var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
-     
       // Now highlight all the stars that's not after the current hovered star
       $(this).parent().children('li.star').each(function(e){
         if (e < onStar) {
           $(this).addClass('hover');
-        }
-        else {
+        }else {
           $(this).removeClass('hover');
         }
       });
-      
     }).on('mouseout', function(){
       $(this).parent().children('li.star').each(function(e){
         $(this).removeClass('hover');
       });
     });
-    
-    
     /* 2. Action to perform on click */
     $('#stars li').on('click', function(){
       var onStar = parseInt($(this).data('value'), 10); // The star currently selected
       var stars = $(this).parent().children('li.star');
-      
       for (i = 0; i < stars.length; i++) {
         $(stars[i]).removeClass('selected');
       }
-      
       for (i = 0; i < onStar; i++) {
         $(stars[i]).addClass('selected');
       }
-      
       // JUST RESPONSE (Not needed)
       var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
       var msg = "";
       if (ratingValue > 1) {
           msg = "Thanks! You rated this " + ratingValue + " stars.";
-      }
-      else {
+      }else {
           msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
       }
       responseMessage(msg);
       ajax(ratingValue);
     });
-    
-    
   });
-  
-  
   function responseMessage(msg) {
     $('.success-box').fadeIn(200);  
     $('.success-box div.text-message').html("<span>" + msg + "</span>");
   }
-
   function ajax(value) {
       $.ajax({
         type:"GET",
         url:"/biblioteca/"+$("#ids").text()+"/star/"+value
-      }).done(function () {
-          
-      });
+      }).done(function () {});
   }
